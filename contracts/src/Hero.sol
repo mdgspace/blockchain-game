c3d91d1694042a7df3611b4a4d2446592cd09b20
Vinayak-RZ <vinayakraizada@gmail.com>
2025-06-01 00:33:46 +0530

    feat:add mintHero function in Hero.sol

--- contracts/src/Hero.sol
+++ contracts/src/Hero.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Hero {
-    //make mutable nft
+
     struct offensivestats{
         uint32 damage;
         uint32 attackspeed;
@@ -16,8 +15,8 @@ contract Hero {
         uint32 maxhealth;
         uint32 defense;
         uint32 healthregeneration;
-        uint32 stunresistance;
-        string[] resistances;             //element we resist
+        uint32[] resistances;            //elements and stun resist
+        // 0 - stun ,1-fire ...
     }
     struct specialstats{
         uint32 maxenergy;
@@ -33,20 +32,45 @@ contract Hero {
     }
     struct race{
         string name;
-        string resistance;
     }
     struct Herodata{
         string playername;
         string playerID;  
-        uint32 level;  
-        string[] activeskills;       //maybe,here it should actually be an object of class skill which be defind in skillproposal(haven't decided actually)
-        string[] passiveskills;     //ideation neede on this (making a skill tree)
+        uint32 level;    	// level of that character in the game they are referred to as 'Level'.       
+        string[] equippeditem;
         stats statstable;		// stats statstable; 
-        race racestable;       // race of that character, in the game they are referred to as 'Race'.  	     
-        string[] equippeditems;
+        race racestable;       // race of that character, in the game they are referred to as 'Race'.
         bool isbanned;         // if true the user cannot play with this player.
-    }
+    }   
 
-    //primary stats which go on L1 are:
-    //Secondary stats which go on L1 are:
+    mapping(uint256 => Herodata) public heroData;
+    
+    function mintHero(
+        string memory _playername,
+        string memory _playerID,
+        string memory _raceName,
+        uint32[] memory _resistance,  //ispe ek baar discus kar lena humse
+        string memory tokenURI,
+        uint256 tokenId
+    ) public {
+
+        string[] memory emptyArray;
+
+        heroData[tokenId] = Herodata({
+            playername: _playername,
+            playerID: _playerID,
+            level: 1,
+            equippeditem: emptyArray,
+            statstable: stats({
+                offstats: offensivestats(5, 100 , 10 , 50),
+                defstats: defensivestats(100, 5, 1, _resistance),
+                specstats: specialstats(100, 10, 100, 10)
+            }),
+            racestable: race(_raceName),
+            isbanned: false
+        });
+
+    }
+    //primary stats which go on L1 are:health,damage,defense,mana,energy
+    //Secondary stats which go on L1 are:all others 
 }
