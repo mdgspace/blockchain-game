03294170309aecde05bfc08b5182052bbf07bba6
Vinayak-RZ <vinayakraizada@gmail.com>
2025-06-21 22:39:10 +0530

    feat(GUI): adds level up page gui with stats and attributes

--- unity/Assets/Scripts/Player/PlayerGUI.cs
+++ unity/Assets/Scripts/Player/PlayerGUI.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class PlayerGUI : MonoBehaviour
 {
     public GameObject Player;
     public Slider HealthSlider;
     public Slider StaminaSlider;
     public Slider ManaSlider;
-    public Text HealthText;
-    public Text EnergyText;
-    public Text ManaText;
+    public TextMeshProUGUI HealthText;
+    public TextMeshProUGUI AttackText;
+    public TextMeshProUGUI DefenseText;
+    public TextMeshProUGUI EnergyText;
+    public TextMeshProUGUI ManaText;
     public float maxHealth, maxEnergy, maxMana;
+    
     private Player player;
     // Update is called once per frame
     void Start()
@@ -19,11 +22,22 @@ public class PlayerGUI : MonoBehaviour
         maxHealth = player.heroData.defensiveStats.maxHealth;
         maxEnergy = player.heroData.specialStats.maxEnergy;
         maxMana = player.heroData.specialStats.maxMana;
+        HealthText.text = player.heroData.defensiveStats.maxHealth.ToString();
+        AttackText.text = player.heroData.offensiveStats.damage.ToString();
+        DefenseText.text = player.heroData.defensiveStats.defense.ToString();
+        EnergyText.text = player.heroData.specialStats.maxEnergy.ToString();
+        ManaText.text = player.heroData.specialStats.maxMana.ToString();
     }
     void Update()
     {
         HealthSlider.value = player.currentHealth / maxHealth;
         StaminaSlider.value = player.currentEnergy / maxEnergy;
         ManaSlider.value = player.currentMana / maxMana;
+        HealthText.text = player.heroData.defensiveStats.maxHealth.ToString();
+        AttackText.text = player.heroData.offensiveStats.damage.ToString();
+        DefenseText.text = player.heroData.defensiveStats.defense.ToString();
+        EnergyText.text = player.heroData.specialStats.maxEnergy.ToString();
+        ManaText.text = player.heroData.specialStats.maxMana.ToString();
+
     }
 }
